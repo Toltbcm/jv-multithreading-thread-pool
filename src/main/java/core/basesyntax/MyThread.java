8e88451ca7331f5091c68eacfb9b9d12e84dd861
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-02-26 15:37:09 +0200

    implement MyThread

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        long delayTime = Math.round(Math.random() * 500);
+        Thread.sleep(delayTime);
+        return String.format("Task duration was %s ms, execution finished at %s",
+                delayTime, LocalDateTime.now());
     }
 }
